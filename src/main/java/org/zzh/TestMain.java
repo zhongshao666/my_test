bd52561ea3e0be9358aca43514983c67a9799a19
zhihao.zhong <y@gal&01qwrP>
2024-06-07 10:48:21 +0800

    b1 7777

--- src/main/java/org/zzh/TestMain.java
+++ src/main/java/org/zzh/TestMain.java
@@ -3,5 +3,8 @@ package org.zzh;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println(666);
+        System.out.println(7777);
+        System.out.println("7777");
+
     }
 }
